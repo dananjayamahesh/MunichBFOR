d08a36b6c40fe390721020f04e93b5429e014bbd
mahesh <mahesh@paraqum.com>
2018-02-09 20:44:56 +0530

    Update bfoa.py and functions

--- SwarmPackagePy/bfoa.py
+++ SwarmPackagePy/bfoa.py
@@ -40,54 +40,47 @@ class bfoa(intelligence.sw):
 
         #C_list = [C - C * 0.9 * i / iteration for i in range(iteration)]
         # C_list = [C for i in range(iteration)]
-        Ped_list = [Ped - Ped * 0.5 * i / iteration for i in range(iteration)]
+        #Ped_list = [Ped - Ped * 0.5 * i / iteration for i in range(iteration)]
 
-        J = [0 for i in range(n)]
-        J_last = J[::1]
-        J_now = J[::1]
-        Pbest = self.__agents[0]
+        J = np.array([function(x) for x in self.__agents])
+        self._points(self.__agents)
+        Pbest = self.__agents[J.argmin()]
         Gbest = Pbest
+
+        #J = [0 for i in range(n)]
+        #J_last = J[::1]
+        #Pbest = self.__agents[0]
+        #Gbest = Pbest
         C_a = [C for i in range(n)]
 
         for l in range(Ned):
             for k in range(Nre):
-
-                #t = l * Nre + k
                 J_chem = [J[::1]]
-                #print (C_a)
+
+                J = np.array([function(x) for x in self.__agents])
+                #self._points(self.__agents)
+                Pbest = self.__agents[J.argmin()]
+                if function(Pbest) < function(Gbest):
+                    Gbest = Pbest
+                J_last = J[::1]
+
                 for j in range(Nc):
-                    # Can Parallelize for each bacteria or individuals
-                    #print (C_a)
-                    J = np.array([function(x) for x in self.__agents])
+
                     self._points(self.__agents)
-                    Pbest = self.__agents[J.argmin()]
-                    if function(Pbest) < function(Gbest):
-                        Gbest = Pbest
-                    J_last = J[::1]
-                    J_now = J[::1]
 
                     for i in range(n):
 
+                        # Can move inside
                         dell = np.random.uniform(-1, 1, dimension)
-                        # C_list[i] = 1 / (1 + (lamda / J[i]))
-                        # Initiate a Tumbling Step
-                        #C_a[i] = 1 / (1 + (lamda / np.abs(J[i])))
                         self.__agents[i] += C_a[i] * \
                             np.linalg.norm(dell) * dell
                         J[i] = function(self.__agents[i])
-                        # self.__agents[i] += C_a[i] * np.linalg.norm(dell) * dell
-
                         # Start Swim Steps
                         for m in range(Ns):
 
-                            # J_now[i] = function(self.__agents[i])
                             # bacteria moves only when objective function is reduced
-
                             if J[i] < J_last[i]:
-                                # Check for last J or nex J
-                                # J_last[i] = J_now[i]
                                 J_last[i] = J[i]
-                                #J_last[i] = J[i]
                                 self.__agents[i] += C_a[i] * np.linalg.norm(dell) \
                                     * dell
                                 J[i] = function(self.__agents[i])
@@ -97,6 +90,7 @@ class bfoa(intelligence.sw):
                                 #dell = np.random.uniform(-1, 1, dimension)
                                 #self.__agents[i] += C_a[i] * np.linalg.norm(dell) * dell
 
+                    # Make lgorithm faster
                     #J = np.array([function(x) for x in self.__agents])
                     J_chem += [J]
 
@@ -105,6 +99,8 @@ class bfoa(intelligence.sw):
                 J_chem = np.array(J_chem)
 
                 J_health = [(sum(J_chem[:, i]), i) for i in range(n)]
+
+                # Sorting: Performance#1
                 J_health.sort()
                 alived_agents = []
                 for i in J_health:
